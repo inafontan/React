5106de1e82ba9ce5236202d30b303079874d8a42
inafontan@gmail.com <you@example.com>
2022-04-23 18:26:48 -0300

    Desafío: Menú e-commerce

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+import { toBePartiallyChecked } from '@testing-library/jest-dom/dist/matchers';
+import CartWidget from './components/CartWidget';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1> Hola Mundo! Empezando con React </h1>
-          <h2> Desafío: Crear la App utilizando el CLI </h2>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+  const stylesP = {
+    border: 'solid 3px black',  
+    padding: '10px',  
+    margin: '10px',
+}
+
+  return (                      //dentro del return le decimos que se va a ver
+    <>
+      <NavBar />
+       
+      
+      <h1 style={{ background: 'red', padding: '5px', margin: '5px'}}>Hola!</h1>
+      <p style={stylesP}>Desafío Crear la App utilizando Cli - Clase 1/2</p>
+      <button class="btn">Click Aquí</button>
+      
+      <ItemListContainer saludo='Bienvenido a la tienda!' />
+    </>
   );
 }
 
 export default App;
+
