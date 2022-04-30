569564e38822c86d4b43864937fbc6f8d911dcf2
inafontan@gmail.com <you@example.com>
2022-04-30 16:33:51 -0300

    Desafío: Catálogo con MAPS y Promises

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,9 @@
  const ItemList = ( {itemInfo} ) => {
   return (
-    <div style={{border:'solid purple 5px', margin:'10px', padding:'5px', width: '250px', height:'300px', display:'flex', flexDirection:'column', alignItems:'center'}}>
+    <div style={{
+      border: 'solid purple 5px', margin: '10px', padding: '5px', width: '250px', height: '300px',
+      display: 'flex', flexDirection: 'column', alignItems: 'center',
+      boxShadow: ' 0 0.125rem 0.3125rem 0 rgba(2, 2, 0, 0.678)'}}>
       <img src={itemInfo.imgUrl}/>
       <h1> <strong> {itemInfo.name}</strong></h1>
       <h2> <strong> {itemInfo.precio}</strong></h2>
