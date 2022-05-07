89508968de282907d6e5819cb110abd022ee6b6e
inafontan@gmail.com <you@example.com>
2022-05-07 13:16:06 -0300

    Correcciones Desafío: Detalle Producto

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export const Item = ({ item }) => {
     return <div style={{
-      border: 'solid purple 5px', margin: '10px', padding: '5px', width: '250px', height: '300px',
+      border: 'solid purple 5px', margin: '10px', padding: '5px', width: '250px', height: '250px',
       display: 'flex', flexDirection: 'column', alignItems: 'center',
       boxShadow: ' 0 0.125rem 0.3125rem 0 rgba(2, 2, 0, 0.678)'}}>
       <img src={item.imgUrl}/>
-      <h1> <strong> {item.name}</strong></h1>
-      <h2> <strong> {item.precio}</strong></h2>
+      {/* <h1> <strong> {item.name}</strong></h1>
+      <h2> <strong> {item.precio}</strong></h2> */}
     </div> 
     }
