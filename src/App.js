3d5ebb30328a67019e5cf4b7388bef3eaa135302
inafontan@gmail.com <you@example.com>
2022-06-15 00:01:37 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import NavBar from './components/NavBar';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import ItemListContainer  from './components/ItemListContainer';
-import ItemDetailContainer  from './components/ItemDetailContainer';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
 import CartContextProvider from './components/context/CartContext';
 import AppContextProvider from './components/context/AppContext';
 import CartView from './components/CartView';
 import dataToFirestore from './data/dataToFirestore';
 function App() {
-  return (
-    <>
-      <AppContextProvider>
-        <CartContextProvider>
-          <BrowserRouter>   
-            <NavBar />
-            <Routes>
-              <Route path="/" element={<ItemListContainer greeting={'Inicio'} />} />
-              <Route path="/categoria/:catId" element={<ItemListContainer greeting={'Vista de categoría'} />} />
-              <Route path="/product/:id" element={<ItemDetailContainer />} />
-              <Route path="/cart" element={<CartView />} />
-            </Routes>
-          </BrowserRouter>
-        </CartContextProvider>
-      </AppContextProvider>
-    </>
-  )
+    return (
+        <>
+            <AppContextProvider>
+                <CartContextProvider>
+                    <BrowserRouter>
+                        <NavBar/>
+                        <Routes>
+                            <Route path="/"
+                                element={<ItemListContainergreeting={'Inicio'}/>
+    )
 }
 
 export default App;
